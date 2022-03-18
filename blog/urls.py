e0e054b00e6259a08f765ae6dda9a6515ae23842
Lucy-Glanville-Brown <lucyglanvillebrown@gmail.com>
2022-03-18 21:34:12 +0000

    Add PostDeleteView and create post_confirm_delete.html2

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,4 @@
-from .views import PostCreateView, PostUpdateView
+from .views import PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 from django.urls import path
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('<slug:slug>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('<slug:slug>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views import generic, View
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView, DeleteView
 from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm, PostForm
@@ -105,6 +105,18 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'post_confirm_delete.html'
+    success_url = 'home'
 
     def test_func(self):
         post = self.get_object()
