a8e4bff0a263349aedf4512c3ed6d9f0be4258ef
Lucy-Glanville-Brown <lucyglanvillebrown@gmail.com>
2022-03-26 09:08:43 +0000

    Removed commented out code

--- wunderlust/settings.py
+++ wunderlust/settings.py
@@ -100,21 +100,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'wunderlust.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
